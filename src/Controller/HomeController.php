47f39b7f136a4d0e25bece6f15025cc31b3bf772
fredoh91 <fredoh@gmail.com>
2019-03-26 11:04:54 +0100

    Video 2/17 - 6m45

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,9 +1,26 @@
 <?php
 namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
 class HomeController {
     
-    public function index () {
-        
+    
+    /**
+     *
+     * @var Environment 
+     */
+    private $twig;
+    
+    
+    public function __construct($twig)
+    {
+        $this->twig=$twig;
+    }
+    public function index (): Response
+    {
+        return new Response ($this->twig->render('pages/home.html.twig'));
     }
 }
 /* 
